99826b6d52421f02cdb7938692d4ccc3a8a5007a
fieldbox <35745665+fieldbox@users.noreply.github.com>
2025-04-15 17:58:11 +0100

    Add framebuffer support

--- boot.s
+++ boot.s
@@ -1,17 +1,40 @@
 .set MAGIC, 0xE85250D6 /* multiboot magic number */
 .set ARCH, 0 /* architecture 0 is 32-bit i386 */
-.set LENGTH, 24 /* length of multiboot header */
-.set CHECKSUM, -(MAGIC + ARCH + LENGTH) /* checksum to prove we are multiboot */ 
+.set HEADER_LENGTH, multiboot_header_end - multiboot_header_start /* length of multiboot header */
+.set CHECKSUM, -(MAGIC + ARCH + HEADER_LENGTH) /* checksum to prove we are multiboot */ 
+
+.set FRAMEBUFFER_TAG_TYPE, 5
+.set FRAMEBUFFER_TAG_FLAGS, 0
+.set FRAMEBUFFER_TAG_SIZE, framebuffer_tag_end - framebuffer_tag_start 
+.set FRAMEBUFFER_TAG_WIDTH, 0
+.set FRAMEBUFFER_TAG_HEIGHT, 0
+.set FRAMEBUFFER_TAG_DEPTH, 0
 
 /* multiboot2 header, in its own section so we can put it in the first 8KiB of the kernel */
 .section .multiboot
-.align 4
+.align 8
+multiboot_header_start:
 .long MAGIC
 .long ARCH
-.long LENGTH
+.long HEADER_LENGTH
 .long CHECKSUM
+
+.align 8
+
+framebuffer_tag_start:
+.short FRAMEBUFFER_TAG_TYPE
+.short FRAMEBUFFER_TAG_FLAGS
+.long FRAMEBUFFER_TAG_SIZE
+.long FRAMEBUFFER_TAG_WIDTH
+.long FRAMEBUFFER_TAG_HEIGHT
+.long FRAMEBUFFER_TAG_DEPTH
+framebuffer_tag_end:
+
+.align 8
+
 .long 0
 .long 8
+multiboot_header_end:
 
 /* defining the stack */
 .section .bss /* in its own section so we can mark it nobits to reduce kernel file size */
@@ -24,9 +47,11 @@ stack_top:
 .global _start
 .type _start, @function
 _start:
+	
 	/* set up the stack by setting the stack pointer register esp*/
 	mov $stack_top, %esp
 
+	push %ebx
 	call kernel_main
 
 	cli
